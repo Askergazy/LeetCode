7fe4fe1e5d998018e01f486baeba138f83776b85
askar <askaraskergazy@gmail.com>
2024-05-23 18:20:28 +0500

    solved 2469 Convert the Temperature

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,19 @@
+import java.util.Arrays;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
 
+
+        String s = "rwohu";
+        String t = "rwuoh";
+
+
+        Solution solution = new Solution();
+
+        double celc = 36.50;
+
+        System.out.println(Arrays.toString(solution.convertTemperature(celc)));
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -20,24 +20,34 @@ public class Solution {
     public int findPermutationDifference(String s, String t) {
 
 
-        Map<Character,Integer> nm=new HashMap<>();
-        for(int i=0;i<s.length();i++)
-        {
-            char c=s.charAt(i);
-            nm.put(c,i);
+        Map<Character, Integer> nm = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            nm.put(c, i);
         }
-        Map<Character,Integer> kk=new HashMap<>();
-        for(int i=0;i<t.length();i++)
-        {
-            char c=t.charAt(i);
-            kk.put(c,i);
+        Map<Character, Integer> kk = new HashMap<>();
+        for (int i = 0; i < t.length(); i++) {
+            char c = t.charAt(i);
+            kk.put(c, i);
         }
-        int ans=0;
-        for(char c:nm.keySet())
-        {
-            ans=ans+Math.abs(nm.get(c)-kk.get(c));
+        int ans = 0;
+        for (char c : nm.keySet()) {
+            ans = ans + Math.abs(nm.get(c) - kk.get(c));
         }
         return ans;
+    }
+
+    public double[] convertTemperature(double celsius) {
+        double[]ans = new double[2];
+
+        ans[0] =  celsius +  273.15;
+        ans[1] = celsius * 1.80 + 32.00;
+
+
+
+        return ans;
+
+
     }
 
 
