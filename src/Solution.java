fd7651d3abb96f9488b313fa3be3a6cdaa1fdd2d
askar <askaraskergazy@gmail.com>
2024-05-27 16:27:09 +0500

    solved 1720 Decode XORed Array

--- src/Solution.java
+++ src/Solution.java
@@ -70,8 +70,25 @@ public class Solution {
     public int numberOfMatches(int n) {
 
 
+        return n - 1;
+    }
 
-        return n-1;
+
+    public int[] decode(int[] encoded, int first) {
+
+        int[] result = new int[encoded.length + 1];
+
+        result[0] = first;
+
+        for (int i = 0; i < encoded.length; i++) {
+
+            result[i + 1] = result[i] ^ encoded[i];
+
+
+        }
+
+
+        return result;
     }
 
 
